bb1ac6aa76f0c65a122bb630fd5be7264c3fa41d
Lichun <lcbiotech@126.com>
2025-02-23 12:05:33 +0900

    Test Submission

--- src/6.pbr/2.2.1.ibl_specular/ibl_specular.cpp
+++ src/6.pbr/2.2.1.ibl_specular/ibl_specular.cpp
@@ -13,6 +13,8 @@
 
 #include <iostream>
 
+// Test My Submission
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
